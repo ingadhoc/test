1846175ea51791d74a1168a64b0626b26975ebe6
Virginia <vib@adhoc.inc>
2025-11-18 18:11:02 +0000

    Test

--- partner_sales_abc/models/res_partner.py
+++ partner_sales_abc/models/res_partner.py
@@ -2,4 +2,10 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import models
+
+from odoo import fields, models
+
+
+class partner(models.Model):
+    _inherit = "res.partner"
+    abc_sales_amount = fields.Char("ABC Sales amount")
