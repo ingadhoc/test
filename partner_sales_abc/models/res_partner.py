22b7c10ac2a59a805d476ce2ba2a459f65b6a1e1
Virginia <vib@adhoc.inc>
2025-11-26 16:36:57 +0000

    Update translation dates and add 'Otro Test' field description

--- partner_sales_abc/models/res_partner.py
+++ partner_sales_abc/models/res_partner.py
@@ -10,3 +10,4 @@ class partner(models.Model):
     _inherit = "res.partner"
     abc_sales_amount = fields.Char("ABC Sales amount asd")
     test_field = fields.Char()
+    otro_test = fields.Char()
