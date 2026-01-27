a86605ca283cd1ed155b52c95c8e362a12238123
roboadhoc <roboadhoc@adhoc.com.ar>
2026-01-27 19:31:52 +0000

    [BOT] Bump version: partner_sales_abc 13.0.1.3.0, mail_ux 19.0.1.3.0
    Merged: ingadhoc/test#104

--- partner_sales_abc/__manifest__.py
+++ partner_sales_abc/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Partner Sales ABC",
-    "version": "13.0.1.2.0",
+    "version": "13.0.1.3.0",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
     "license": "AGPL-3",
